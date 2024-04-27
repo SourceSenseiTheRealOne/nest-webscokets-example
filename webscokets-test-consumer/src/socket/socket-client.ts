9ad86d4a52410120927b1a855141c682df06d711
SourceSenseiTheRealOne <source.sensei@proton.me>
2024-04-27 14:07:53 +0100

    first commit

---
+++ webscokets-test-consumer/src/socket/socket-client.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { io, Socket } from 'socket.io-client';
+
+@Injectable()
+export class SocketClient implements OnModuleInit {
+  public socketClient: Socket;
+
+  constructor() {
+    this.socketClient = io('http://localhost:3000');
+  }
+
+  onModuleInit() {
+    this.registerConsumerEvents();
+  }
+
+  private registerConsumerEvents() {
+    this.socketClient.on('connect', () => {
+      console.log('Connected to Gateway');
+    });
+
+    this.socketClient.on('onMessage', (data: any) => {
+      console.log(data);
+    });
+  }
+}
---
+++ webscokets-test/src/gateway/gateway.ts
@@ -0,0 +1,30 @@
+import { OnModuleInit } from '@nestjs/common';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway()
+export class MyGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log('connected');
+    });
+  }
+
+  @SubscribeMessage('newMessage')
+  onNewMessage(@MessageBody() body: any) {
+    console.log(body);
+    this.server.emit('onMessage', {
+      message: 'Server is transmitting',
+      content: body,
+    });
+  }
+}
